783039b95c702f086601c3dc4e0a79490b8e933d
tvp <pvtinh1996@gmail.com>
2018-12-13 09:19:49 +0700

    fix env name BASE_PATH to REACT_APP_API_URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 import utils from "./utils";
 
-const KEY_PERSIST_STORE = 'fight-booking-app';
+const KEY_PERSIST_STORE = 'react-app-template-v16';
 const API_URL = process.env.REACT_APP_API_URL;
 const BASE_PATH =  process.env.REACT_APP_BASE_PATH;
 
